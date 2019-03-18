f66c859b0ed8214c7bdfafa9480f38d692e56736
PiotrBujak <Piotrus.bujak@gmail.com>
2019-03-18 09:51:44 +0100

    init

---
+++ src/test/java/akademiakodu/nasaapp/NasaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package akademiakodu.nasaapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NasaAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
